2cdd0d133f7dffa9a94732d2aa5db83bcc875179
LUKARYU <fbfmzk1990>
2022-01-05 01:58:57 +0900

    gson: class를 만들지 않고 직접 json 만들어서 보내게 해주는 라이브러리

--- src/main/java/com/example/demo/info/InfoController.java
+++ src/main/java/com/example/demo/info/InfoController.java
@@ -6,6 +6,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.info.model.Project;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
 @RestController
 public class InfoController {
@@ -17,5 +19,25 @@ public class InfoController {
 		project.author = "jydev";
 		project.createdDate = new Date();
 		return project;
+	}
+	
+	@GetMapping("/info2")
+	public String customJson() {
+		JsonObject jo = new JsonObject();
+		
+		jo.addProperty("projectName", "apiDemo");
+		jo.addProperty("author", "jydev");
+		jo.addProperty("createdDate", new Date().toString());
+		
+		JsonArray ja = new JsonArray();
+		for(int i=0; i<5; i++) {
+			JsonObject jObj = new JsonObject();
+			jObj.addProperty("prop"+i, i);
+			ja.add(jObj);
+		}
+		
+		jo.add("follower", ja);
+		
+		return jo.toString();
 	}
 }
